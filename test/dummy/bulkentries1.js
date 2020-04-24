4d6cb32c2afc6f89530b5994786ae2a9644522b1
Abhinav Gupta <abhinav.gupta@contentstack.com>
2020-04-25 01:28:23 +0530

    Added tests and resolved eslint errors

--- test/dummy/bulkentries1.js
+++ test/dummy/bulkentries1.js
@@ -2,17 +2,94 @@ module.exports = {
   entries: [{
     uid: 'dummyEntryId',
     title: 'my title',
-    publish_details:[{
-    	environment:'dummyE',
-    	locale:'en-us'
-    }]
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
   }, {
     uid: 'dummyEntryId2',
     title: 'my title 2',
-    publish_details:[{
-    	environment:'dummyE',
-    	locale:'en-us'
-    }]
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId3',
+    title: 'my title 3',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId4',
+    title: 'my title 4',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId5',
+    title: 'my title 5',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId6',
+    title: 'my title 6',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId7',
+    title: 'my title 7',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId8',
+    title: 'my title 8',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId9',
+    title: 'my title 9',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId10',
+    title: 'my title 10',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId11',
+    title: 'my title 11',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId12',
+    title: 'my title 12',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
+  }, {
+    uid: 'dummyEntryId13',
+    title: 'my title 13',
+    publish_details: [{
+      environment: 'dummyE',
+      locale: 'en-us',
+    }],
   }],
-  count :4
+  count: 13,
 };
